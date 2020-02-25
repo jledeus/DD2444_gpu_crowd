c633d7269f9129d72b0797c662568a75e4711f69
root <root@LAPTOP-8HSJVPKC.localdomain>
2020-02-25 09:23:53 +0100

    Initial commit

---
+++ init_canvas.js
@@ -0,0 +1,79 @@
+
+// The initialization is partly taken from a webgl2 tutorial, http://webglworkshop.com/28/01-triangle-webgl2.html
+
+var canvas = document.getElementById("webgl-canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var gl = canvas.getContext("webgl2");
+if (!gl) {
+    console.error("WebGL 2 not available");
+    document.body.innerHTML = "This example requires WebGL 2 which is unavailable on this system."
+}
+
+// White background
+gl.clearColor(1, 1, 1, 1);
+
+// Load shaders from src in index.html
+var vsSource = document.getElementById("vs").text.trim();
+var fsSource = document.getElementById("fs").text.trim();
+var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertexShader, vsSource);
+gl.compileShader(vertexShader);
+
+if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    console.error(gl.getShaderInfoLog(vertexShader));
+}
+
+var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragmentShader, fsSource);
+gl.compileShader(fragmentShader);
+
+if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    console.error(gl.getShaderInfoLog(fragmentShader));
+}
+
+var program = gl.createProgram();
+gl.attachShader(program, vertexShader);
+gl.attachShader(program, fragmentShader);
+gl.linkProgram(program);
+
+if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.error(gl.getProgramInfoLog(program));
+}
+
+gl.useProgram(program);
+
+/////////////////////
+// Placeholder geometry for one person
+/////////////////////
+
+var triangleArray = gl.createVertexArray();
+gl.bindVertexArray(triangleArray);
+
+var positions = new Float32Array([
+    -0.05, -0.05, 0.0,
+    -0.05, 0.05, 0.0,
+    0.05, -0.05, 0.0,
+    0.05, 0.05, 0.0,
+]);
+
+var positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(0);
+
+var colors = new Float32Array([
+    0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0,
+
+]);
+
+var colorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
+gl.vertexAttribPointer(1, 4, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(1);
